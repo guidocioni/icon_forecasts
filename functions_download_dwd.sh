b4c18ecf117e552ff420ba38e412aa8603ebbce1
Guido Cioni <guidocioni@gmail.com>
2020-10-02 09:37:51 +0200

    Added 3 parallel connections for 3D variable download to speed up

--- functions_download_dwd.sh
+++ functions_download_dwd.sh
@@ -27,7 +27,14 @@ export -f download_merge_2d_variable_icon_eu
 download_merge_3d_variable_icon_eu()
 {
 	filename="icon-eu_europe_regular-lat-lon_pressure-level_${year}${month}${day}${run}_*_${1}.grib2"
-	wget -b -r -nH -np -nv -nd --reject "index.html*" --cut-dirs=3 -A "${filename}.bz2" "https://opendata.dwd.de/weather/nwp/icon-eu/grib/${run}/${1,,}/"
+	# 3 multiple connections so it should be faster
+	filenames="icon-eu_europe_regular-lat-lon_pressure-level_${year}${month}${day}${run}_0[0-4][0-9]_*_${1}.grib2"
+	wget -b -r -nH -np -nv -nd --reject "index.html*" --cut-dirs=3 -A "${filenames}.bz2" "https://opendata.dwd.de/weather/nwp/icon-eu/grib/${run}/${1,,}/"
+	filenames="icon-eu_europe_regular-lat-lon_pressure-level_${year}${month}${day}${run}_0[5-9][0-9]_*_${1}.grib2"
+	wget -b -r -nH -np -nv -nd --reject "index.html*" --cut-dirs=3 -A "${filenames}.bz2" "https://opendata.dwd.de/weather/nwp/icon-eu/grib/${run}/${1,,}/"
+	filenames="icon-eu_europe_regular-lat-lon_pressure-level_${year}${month}${day}${run}_1[0-9][0-9]_*_${1}.grib2"
+	wget -b -r -nH -np -nv -nd --reject "index.html*" --cut-dirs=3 -A "${filenames}.bz2" "https://opendata.dwd.de/weather/nwp/icon-eu/grib/${run}/${1,,}/"
+	#
 	parallelized_extraction ${filename}
 	cdo merge ${filename} ${1}_${year}${month}${day}${run}_eur.grib2
 	rm ${filename}
